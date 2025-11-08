b456768f074450f3a3a33604381ec4cda992aa24
sellwia <Sellydwikurnia@gmail.com>
2025-11-08 13:14:50 +0700

    buat page splash scaffold awal

---
+++ lib/screen/splash_screen_1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SpashScreen1 extends StatelessWidget {
+  const SpashScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold();
+  }
+}
