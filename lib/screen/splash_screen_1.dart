f217fc1cd0c88d302482443157d98880fc5161ad
sellwia <Sellydwikurnia@gmail.com>
2025-11-08 13:21:30 +0700

    buat image bulat dalam column

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
-class SpashScreen1 extends StatelessWidget {
-  const SpashScreen1({super.key});
+class SplashScreen1 extends StatelessWidget {
+  const SplashScreen1({super.key});
 
   @override
   Widget build(BuildContext context) {
-     return Scaffold();
+     return Scaffold(
+      body: Column(
+          // Todo:
+          // karna akan membuat multiple widget
+          // maka membuat children yang bisa menampung banyak
+          //widget children
+
+          //membuat image bulat
+          //membuat title dengan warna hitam
+          //membuat subtitle
+          //membuat 3 bullet kecil
+          //mulai dari warna terang awal
+          //membuat button continue
+          children: [
+          SizedBox(height:50),
+          Container (
+            width : 250, 
+            height : 250,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.amber),
+          ),
+          //tambahan untuk widget lainnya
+          ],
+        ),
+     );
   }
 }
