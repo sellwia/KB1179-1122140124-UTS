f2265dad6b82dd98b5f6737f574e3f4c1d90f0b8
sellwia <Sellydwikurnia@gmail.com>
2025-11-08 16:15:37 +0700

    buat headline dan subtitle

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -18,7 +18,24 @@ class Login extends StatelessWidget {
                 color: Colors.amber,
               ),
               SizedBox(height: 10),
+            Text(
+              "Selamat Datang",
+              style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.normal),
+            ),
+
+            SizedBox(height: 20), //jarak 20 antara lingkaran dan teks
+
+            Text(
+              "Silahkan Login Untuk Melanjutkan",
+              style: TextStyle(
+                fontSize: 15.0,
+                color: Colors.amber,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
             ],
+
           ),
         ),
       ),
